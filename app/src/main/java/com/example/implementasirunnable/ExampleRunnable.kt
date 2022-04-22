fe9f8a52b3cd679371dd302ab3128eae3e08690a
Eka Juliawati <ekajuliawati63@gmail.com>
2022-04-22 15:05:49 +0700

    Implementasi Runnable

---
+++ app/src/main/java/com/example/implementasirunnable/ExampleRunnable.kt
@@ -0,0 +1,20 @@
+package com.example.implementasirunnable
+
+import android.util.Log
+class ExampleRunnable : Runnable {
+    var seconds = 0
+    val TAG = "MainActivity"
+    constructor(seconds: Int) {
+        this.seconds = seconds
+    }
+    override fun run() {
+        for (i in 1 until seconds) {
+            Log.d(TAG, "startThread: $i")
+            try {
+                Thread.sleep(1000)
+            } catch (ie: InterruptedException) {
+                ie.printStackTrace()
+            }
+        }
+    }
+}
